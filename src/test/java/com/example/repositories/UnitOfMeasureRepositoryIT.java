2907bce99bcc05709f3bab5f1f35d3803ef26eee
Nikita Koynov <nikicha03@gmail.com>
2020-06-30 16:16:22 +0300

    mongodb integration test fix

--- src/test/java/com/example/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/example/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,11 +1,13 @@
 package com.example.repositories;
 
+import com.example.bootstrap.DataLoader;
 import com.example.domain.UnitOfMeasure;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,19 +15,27 @@ import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by jt on 6/17/17.
- */
-@Ignore
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
     @Before
     public void setUp() throws Exception {
+        recipeRepository.deleteAll();
+        categoryRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+
+        DataLoader dataLoader = new DataLoader(categoryRepository, recipeRepository, unitOfMeasureRepository);
+        dataLoader.onApplicationEvent(null);
     }
 
     @Test
